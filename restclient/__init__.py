62bd3f27e4d7d8704035771245ad76ca9016dd14
Igor Voropaev <snakework10@gmail.com>
2022-03-25 09:47:59 +0500

    v2.4.2 - Исправлен динамический поиск версии

--- restclient/__init__.py
+++ restclient/__init__.py
@@ -2,7 +2,7 @@ from .client import RestClient, AsyncRestClient
 from .response import RestResponse, ErrorResponse
 from .exceptions import RestQueryError
 
-__version__ = '2.4.0'
+__version__ = "2.4.2"
 __all__ = [
     'RestClient',
     'AsyncRestClient',
